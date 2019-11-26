c520cf6780b1bb5746f580f010a162ca85990b19
Talbekk <mt.campbell@outlook.com>
2019-11-26 14:47:29 +0000

    first mvp

--- src/app.js
+++ src/app.js
@@ -1,7 +1,28 @@
 import Vue from 'vue';
 
 document.addEventListener('DOMContentLoaded', () => {
-new Vue ({
+  new Vue({
     el: '#app',
+    data: {
+      countryList: null,
+      totalPopulation: 0
+    },
+    mounted() {
+      this.fetchCountries();
+      this.fetchTotalPopulation();
+    },
+    computed: {
+      fetchTotalPopulation: function () {
+      const result = this.countryList.reduce((total, country) => total += country.population, 0);
+      this.totalPopulation = result;
+    }
+  },
+    methods: {
+      fetchCountries: function () {
+          const request = fetch("https://restcountries.eu/rest/v2/all")
+          .then(response => response.json())
+          .then(data => this.countryList = data);
+      }
+    }
 });
 });
