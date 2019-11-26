47879bafa03fb6cbf4a4b493e2ab2ea573b00af4
Talbekk <mt.campbell@outlook.com>
2019-11-26 16:39:45 +0000

    Finished MVP

--- src/app.js
+++ src/app.js
@@ -4,21 +4,17 @@ document.addEventListener('DOMContentLoaded', () => {
   new Vue({
     el: '#app',
     data: {
-      countryList: null,
+      countryList: [],
       totalPopulation: 0,
-      chosenCountry: {
-        name: "",
-        flag: null
-      }
+      chosenCountry: null,
+      favoriteCountries: []
     },
     mounted() {
       this.fetchCountries();
-      this.fetchTotalPopulation());
     },
     computed: {
       fetchTotalPopulation: function () {
-      const result = this.countryList.reduce((total, country) => total += country.population, 0);
-      return this.totalPopulation = result;
+      return this.countryList.reduce((total, country) => total += country.population, 0);
     }
   },
     methods: {
@@ -27,11 +23,10 @@ document.addEventListener('DOMContentLoaded', () => {
           .then(response => response.json())
           .then(data => this.countryList = data);
       },
-      getCountry: function (name) {
-        const result = this.countryList.find(country => country.name === name);
-        this.chosenCountry.name = result.name;
-        this.chosenCountry.flag = result.flag;
+      addCountry: function() {
+          this.favoriteCountries.push(this.chosenCountry);
       }
+
 
     }
 });
